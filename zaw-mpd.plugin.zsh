c4d81fb0c73d4e34cf60a906734f5c62fbee7bb9
William G Hatch <willghatch@gmail.com>
2015-09-18 14:01:59 -0600

    test that zaw exists first -- basically copy termoshtt's init file

--- zaw-mpd.plugin.zsh
+++ zaw-mpd.plugin.zsh
@@ -1,3 +1,9 @@
-for mpdfile in $(ls "${${0:A}:h}"/sources); do
-    source "${${0:A}:h}/sources/$mpdfile"
-done
+if [[ -n $(declare -f -F zaw-register-src) ]]; then
+    here=${${0:A}:h}
+    for fn in $(ls $here/sources/); do
+        source $here/sources/$fn
+    done
+else
+    echo "zaw-mpd plugin not loaded since zaw is not loaded."
+    echo "Please load zaw (https://github.com/zsh-users/zaw) first."
+fi
