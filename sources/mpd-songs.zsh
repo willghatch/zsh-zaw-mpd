77b11750797abe99cc153026581760c58b97732d
William G Hatch <willghatch@gmail.com>
2015-09-20 10:57:33 -0600

    add action to replace buffer with a command to add the parent dir
    this allows the adding of the parent dir of the song, or, with some
    editing, one of its ancestors

--- sources/mpd-songs.zsh
+++ sources/mpd-songs.zsh
@@ -10,12 +10,16 @@ function zaw-src-mpd-songs() {
     fi
     : ${(A)candidates::=${(f)buf}}
     : ${(A)cand_descriptions::=${(f)buf}}
-    actions=( zaw-mpc-add-to-playlist )
-    act_descriptions=( "add to playlist" )
+    actions=( zaw-mpc-add-to-playlist zaw-mpc-add-parent-dir-setup )
+    act_descriptions=( "add to playlist" "replace buffer to add parent dir" )
 }
 
 zaw-mpc-add-to-playlist(){
     $=ZAW_MPC_COMMAND add "$1"
+}
+
+zaw-mpc-add-parent-dir-setup(){
+    LBUFFER="\$=ZAW_MPC_COMMAND add $(dirname $1)"
 }
 
 zaw-register-src -n mpd-songs zaw-src-mpd-songs
